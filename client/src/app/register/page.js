921148f0f283cc064d09910ff79896b49cf52901
Mishan Adhikari <mishanadhikari8@gmail.com>
2024-06-01 21:37:49 +0545

    validation update

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -7,14 +7,21 @@ import * as Yup from 'yup';
 const register = () => {
   const SignupSchema = Yup.object().shape({
     fullName: Yup.string()
-      .required('Required'),
+      .required('*Required'),
       email: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-      password: Yup.string().min(2, 'Too Short!')
-      .max(50, 'Too Long!').required('Required'),
+      .min(2, '*Too Short!')
+      .max(50, '*Too Long!')
+      .required('*Required'),
+      password: Yup.string().min(2, '*Too Short!')
+      .max(50, 'Too Long!').required('*Required'),
+      phoneNumber: Yup.string().max(20, '*Too Long!').min(2, '*Too Short!')
+      .required('*Required')
+      .matches(/^[0-9]+$/, '*Must be a number'),
+      confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], '*Passwords do not match'),
+      role: Yup.string().required('*Please Select an option')
   });
+
   const formik = useFormik({
     initialValues: {
       phoneNumber: '',
@@ -46,7 +53,8 @@ const register = () => {
              onChange={formik.handleChange}
              value={formik.values.phoneNumber}
             type="text" label = "Phone Number" variant="underlined" className="w-full m-4" placeholder="Enter Phone Number" />
-            
+            <div className='text-red-500 text-small'>{formik.errors?.phoneNumber}</div>
+
             <Input
             id="fullName"
             name="fullName"
@@ -56,25 +64,30 @@ const register = () => {
              variant="underlined" 
              className="w-full m-4"
               placeholder="Enter Name" />
-              <div className='text-red-500'>{formik.errors?.fullName}</div>
+              <div className='text-red-500 text-small'>{formik.errors?.fullName}</div>
+
             <Input
              id="email"
              name="email"
              onChange={formik.handleChange}
              value={formik.values.email}
             type="email" label = "Email" variant="underlined" className="w-full m-4" placeholder="Enter email" />
+            <div className='text-red-500 text-small'>{formik.errors?.email}</div>
+            
             <Input
               id="password"
               name="password"
               onChange={formik.handleChange}
               value={formik.values.password}
             type="password" label = "Password" variant="underlined" className="w-full m-4" placeholder="Enter your password" />
+            
             <Input
                  id="confirmPassword"
                  name="confirmPassword"
                  onChange={formik.handleChange}
                  value={formik.values.confirmPassword}
             type="password" label = "Confirm Password" variant="underlined" className="w-full m-4" placeholder="Enter your password again" />
+           <div className='text-red-500 text-small'>{formik.errors?.confirmPassword}</div>
             <RadioGroup
               label="Select Role"
               orientation="horizontal"
@@ -87,6 +100,8 @@ const register = () => {
               <Radio value="Rider">Rider</Radio>
               <Radio value="Passenger">Passenger</Radio>
             </RadioGroup>
+            <div className='text-red-500 text-small m-1'>{formik.errors?.role}</div>
+            
             <Button  type="submit" className="w-full bg-blue-500 text-white"> Create an account </Button>
             <p className="text-sm m-2 text-center">Already have an account? <a as={Link} href="/" className="text-blue-600 font-semibold hover:underline">Login here</a></p>
           
